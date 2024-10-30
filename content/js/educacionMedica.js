85862580b6a0df5145142f9c90b4f5e2378a705d
Emilio Bensignor <liobensignor@gmail.com>
2024-10-29 22:34:01 -0300

    Maquetacion html Educacion Medica

---
+++ content/js/educacionMedica.js
@@ -0,0 +1,44 @@
+const formacionesContainer = document.getElementById("formacionesContainer");
+
+let formas = [
+  {
+    link: "#",
+    img: "oncoClinica",
+    texto: "Residencia en Oncología clínica",
+  },
+  {
+    link: "#",
+    img: "mastologia",
+    texto: "Residencia en Mastología",
+  },
+  {
+    link: "#",
+    img: "hematologia",
+    texto: "Fellowship en Onco-Hematología",
+  },
+  {
+    link: "#",
+    img: "urologia",
+    texto: "Fellowship en Urología Oncológica",
+  },
+  {
+    link: "#",
+    img: "ortopedia",
+    texto: "Fellowship en Ortopedía Oncológica",
+  },
+  {
+    link: "#",
+    img: "torax",
+    texto: "Fellowship en Cirugía Torácica",
+  },
+  {
+    link: "#",
+    img: "psico",
+    texto: "Fellowship en Psico Oncología",
+  },
+  {
+    link: "#",
+    img: "dxi",
+    texto: "Residencia en DXI",
+  },
+];
