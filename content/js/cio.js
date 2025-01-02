53bf0e8178b8362958b6cbe648efc7ae2fdd2f54
Emilio Bensignor <liobensignor@gmail.com>
2025-01-02 16:13:43 -0300

    Cio Maquetacion Mobile

--- content/js/cio.js
+++ content/js/cio.js
@@ -3,6 +3,10 @@ const aniosTesis = document.getElementById('aniosTesis');
 const bodyTesis = document.getElementById('bodyTesis');
 const aniosTesinas = document.getElementById('aniosTesinas');
 const bodyTesinas = document.getElementById('bodyTesinas');
+const aniosPublicaciones = document.getElementById('aniosPublicaciones');
+const bodyPublicaciones = document.getElementById('bodyPublicaciones');
+const aniosPremios = document.getElementById('aniosPremios');
+const bodyPremios = document.getElementById('bodyPremios');
 
 const proyectos = [
     {
@@ -82,6 +86,71 @@ const tesinas = [
     },
     {
         anio: "2015",
+    },
+];
+const publicaciones = [
+    {
+        anio: "2022",
+        body: [
+            "Mordoh A, Aris M, Carri I, Bravo AI, Podaza E, Pardo JCT, Cueto GR, Barrio MM, Mordoh J. An Update of Cutaneous Melanoma Patients Treated in Adjuvancy With the Allogeneic Melanoma Vaccine VACCIMEL and Presentation of a Selected Case Report With In-Transit Metastases. Front Immunol. 2022 Apr 1;13:842555. doi: 10.3389/fimmu.2022.842555.",
+            "Sifón MR, Marcolini N, Barber MJ, Mclean I, Rizzo MM, Rivero S, Costanzo MV, Nervo A, Crimi G, Perazzo F, Levy EM, Mandó P: “Lack of Prognostic Value of Pretreatment Neutrophil-to-Lymphocyte Ratio in Early Breast Cancer”. Breast Care (Karger) 2022. doi: 10.1159/000525287"
+        ],
+    },
+    {
+        anio: "2021",
+    },
+    {
+        anio: "2020",
+    },
+    {
+        anio: "2019",
+    },
+    {
+        anio: "2018",
+    },
+    {
+        anio: "2017",
+    },
+    {
+        anio: "2016",
+    },
+    {
+        anio: "2015",
+    },
+    {
+        anio: "2014",
+    },
+    {
+        anio: "2013",
+    },
+    {
+        anio: "2012",
+    },
+];
+const premios = [
+    {
+        anio: "2018",
+        body: [
+            {
+                titulo: "IUIS-ALAI-MEXICO-IMMUNOINFORMATICS COURSE.",
+                texto: "Center of Complexity Sciences UNAM, Mexico DF. Abril, 2018. “Evaluation of T-cell response against predicted neoantigens for a patient treated with the CSF-470 vaccine”. Enrique Podaza, IbelCarri, Mariana Aris, Patricio Yankilevich, Morten Nielsen, José Mordoh, María Marcela Barrio. Mención segundo mejor trabajo expuesto por los alumnos del curso.",
+            },
+            {
+                titulo: "SAI - LEONARDO SATZ AWARD.",
+                texto: "Título “The role of BIN1 in the expression of PD-L1 in inflammatory bowel disease and colorectal cancer might explain a differential regulatory t cell activity through the PD-1/PD-L1 axis”. Andrés Machicote, Rodrigo Papa Gobbi, Estrella Levy, Andrés Rocca, Alicia Sambuelli, Martín Yantorno, Gustavo Correa, María Teresa Pombo, Luisina Bruno, Guillermo Docena, Leonardo Fainboim.",
+            },
+            {
+                titulo: "XIX JORNADA ANUAL DE MEDICINA INTERNA “DR. NORBERTO FRAGA” CENTRO DE EDUCACIÓN MEDICA E INVESTIGACIONES CLÍNICAS (CEMIC).",
+                texto: "Primer premio 2018. “Linfocitos periféricos TIM-3+ como potencial biomarcador en pacientes con carcinoma de pulmón no pequeñas células y carcinoma renal” Mandó P; Juliá EP; Rizzo M; Tsou F; Lucca R; Bravo AI; Astorino W; Mordoh J; Martín C and Levy EM.",
+            },
+            {
+                titulo: "PREMIO FESS 2018 A LA EXCELENCIA MÉDICA.",
+                texto: "Otorgado por la Fundación de Estudios para la Salud y la Seguridad Socialal Dr. José Mordoh.",
+            },
+        ],
+    },
+    {
+        anio: "2013",
     },
 ];
 
@@ -202,3 +271,74 @@ botonesAnioTesinas.forEach((boton, index) => {
 });
 
 mostrarContenidoTesinas(0);
+
+/* Publicaciones */
+
+for (let item = 0; item < publicaciones.length; item++) {
+    aniosPublicaciones.innerHTML += `
+        <button class="pill anioPublicaciones column" data-anio="${item}">
+            <h3>${publicaciones[item].anio}</h3>
+        </button>
+    `;
+}
+
+const botonesAnioPublicaciones = document.querySelectorAll('.anioPublicaciones');
+
+function mostrarContenidoPublicaciones(index) {
+    bodyPublicaciones.innerHTML = '';
+    botonesAnioPublicaciones.forEach(btn => btn.classList.remove('pillSeleccionada'));
+    botonesAnioPublicaciones[index].classList.add('pillSeleccionada');
+    if (publicaciones[index].body) {
+        publicaciones[index].body.forEach(parrafo => {
+            const publicacionDiv = document.createElement('div');
+            const pElement = document.createElement('p');
+            pElement.innerHTML = parrafo;
+            publicacionDiv.appendChild(pElement);
+            bodyPublicaciones.appendChild(publicacionDiv);
+        });
+    }
+}
+
+botonesAnioPublicaciones.forEach((boton, index) => {
+    boton.addEventListener('click', () => mostrarContenidoPublicaciones(index));
+});
+
+mostrarContenidoPublicaciones(0);
+
+/* Premios */
+
+for (let item = 0; item < premios.length; item++) {
+    aniosPremios.innerHTML += `
+        <button class="pill anioPremios column" data-anio="${item}">
+            <h3>${premios[item].anio}</h3>
+        </button>
+    `;
+}
+
+const botonesAnioPremios = document.querySelectorAll('.anioPremios');
+
+function mostrarContenidoPremios(index) {
+    bodyPremios.innerHTML = '';
+    botonesAnioPremios.forEach(btn => btn.classList.remove('pillSeleccionada'));
+    botonesAnioPremios[index].classList.add('pillSeleccionada');
+    if (premios[index].body) {
+        premios[index].body.forEach(item => {
+            const premioDiv = document.createElement('div');
+            const h3Element = document.createElement('h3');
+            h3Element.textContent = item.titulo;
+            premioDiv.appendChild(h3Element);
+
+            const pElement = document.createElement('p');
+            pElement.innerHTML = item.texto;
+            premioDiv.appendChild(pElement);
+
+            bodyPremios.appendChild(premioDiv);
+        });
+    }
+}
+
+botonesAnioPremios.forEach((boton, index) => {
+    boton.addEventListener('click', () => mostrarContenidoPremios(index));
+});
+
+mostrarContenidoPremios(0);
